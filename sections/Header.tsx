cdd4dc7beb15129628c63c834d74173ea4247f96
Hovhannes <hovhannes.baghdasaryan.03@gmail.com>
2022-11-27 00:15:03 +0400

    add dark light mode into the app

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import {useTheme} from "next-themes"
 
 import HeaderLogo from "../components/HeaderLogo";
+import {MoonIcon, SunIcon} from "@heroicons/react/solid"
 
-const Header = () => (
-    <header className="border-b-4 border-gray-100">
-        <div className="mx-auto px-24 py-2">
-            <HeaderLogo/>
-        </div>
-    </header>
-);
+const Header = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+
+    const RenderThemeChanger = () => {
+        const currentTheme = theme === "system" ? systemTheme : theme;
+
+        if (currentTheme === "dark") {
+            return (
+                <SunIcon className="w-7 h-7" role="button" onClick={() => setTheme("light")}/>
+            )
+        }
+
+        return <MoonIcon className="w-7 h-7" role="button" onClick={() => setTheme("dark")}/>
+    }
+
+    return (
+        <header className="border-b-4 border-gray-100 dark: border-gray-700">
+            <div className="mx-auto px-24 py-2 flex justify-between items-center">
+                <HeaderLogo/>
+                <RenderThemeChanger/>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
